7c0a5928f9f3510c2908b13c213134ed0dc575b5
Samuli Virtanen <samuliedvin@gmail.com>
2018-12-10 13:11:10 +0200

    Add solution for exercise 6.1. eslint

--- redux-anecdotes-v2/src/App.js
+++ redux-anecdotes-v2/src/App.js
@@ -5,17 +5,17 @@ import AnecdoteList from './components/AnecdoteList'
 
 class App extends React.Component {
 
-  render() {
-    const anecdotes = this.props.store.getState()
-    return (
-      <div>
-        <h1>Programming anecdotes</h1>
-        <Notification />
-        <AnecdoteList store={this.props.store} />
-        <AnecdoteForm store={this.props.store} />
-      </div>
-    )
-  }
+    render() {
+        const anecdotes = this.props.store.getState()
+        return (
+            <div>
+                <h1>Programming anecdotes</h1>
+                <Notification />
+                <AnecdoteList store={this.props.store} />
+                <AnecdoteForm store={this.props.store} />
+            </div>
+        )
+    }
 }
 
 export default App
--- redux-anecdotes-v2/src/index.js
+++ redux-anecdotes-v2/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 import store from './store'
 
 
 const render = () => {
-  ReactDOM.render(
-    <App store={store} />,
-    document.getElementById('root')
-  )
+    ReactDOM.render(
+        <App store={store} />,
+        document.getElementById('root')
+    )
 }
 
 render()
