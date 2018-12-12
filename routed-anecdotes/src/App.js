fa65174daf1844984520a60efa2d992874717cdf
Samuli Virtanen <samuliedvin@gmail.com>
2018-12-12 15:20:45 +0200

    Add solutions for exercises 6.21-6.23

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,91 +1,122 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
+import { ListGroup, ListGroupItem, Grid, Row, Col, Alert, FormGroup, FormControl, ControlLabel, Button, Navbar, NavItem, Nav, Well, Label, Panel } from 'react-bootstrap'
 
 const Menu = () => {
-  const style = {
-    backgroundColor: 'grey',
-    color: 'white',
-    padding: '1rem',
-    borderRadius: 10
-  }
-
   const activeStyle = {
-    backgroundColor: 'pink',
-    color: 'black',
-    padding: '1rem 0 1rem 0'
-  }
+    color: 'white',
+    textShadow: '0px 0px 10px #fff'
 
-  return (
-  <div style={style}>
-    <NavLink exact activeStyle={activeStyle} to="/">anecdotes</NavLink>&nbsp;
-    <NavLink exact activeStyle={activeStyle} to="/create">create new</NavLink>&nbsp;    
-    <NavLink exact activeStyle={activeStyle} to="/about">about</NavLink>    
-  </div>
+  }
+  return(
+    <div>
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            Anecdote app                
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem href="#">
+              <NavLink exact activeStyle={activeStyle} to="/">Anecdotes</NavLink>
+            </NavItem>
+            <NavItem href="#">
+              <NavLink exact activeStyle={activeStyle} to="/create">Create new</NavLink> 
+            </NavItem>
+            <NavItem href="#">
+              <NavLink exact activeStyle={activeStyle} to="/about">About</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   )
 }
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
-    <ul>
+    <ListGroup>
       {anecdotes.map(anecdote => 
-        <li key={anecdote.id}>
+        <ListGroupItem key={anecdote.id}>
           <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
-        </li>
+        </ListGroupItem>
       )}
-    </ul>  
+    </ListGroup>  
   </div>
 )
 
 const Anecdote = ({anecdote}) => {
   return(
   <div>
-    <h2>{anecdote.content}</h2>
-    <div><p>this anecdote has {anecdote.votes} votes</p></div>
-    <div><p>For more info, visit <a href={anecdote.info}>{anecdote.info}</a></p></div>
+    <Panel>
+      <Panel.Heading>
+        <Panel.Title componentClass="h3">"{anecdote.content}"</Panel.Title>
+      </Panel.Heading>
+      <Panel.Body>
+        <p>This anecdote has <Label>{anecdote.votes}</Label> votes</p>
+        <p>For more info, visit <a href={anecdote.info}>{anecdote.info}</a></p>
+      </Panel.Body>
+    </Panel>
   </div>
 )}
 
 const About = () => (
   <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
-      An anecdote is "a story with a point."</em>
+    <Grid>
+      <Row>
+        <h2>About anecdote app</h2>
+      </Row>
+      <Row>
+        <Col xs={12} md={8}>
+          <p>According to Wikipedia:</p>
+          
+          <em>An anecdote is a brief, revealing account of an individual person or an incident. 
+            Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
+            such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+            An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+          <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+        </Col>
+        <Col xs={6} md={4}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Alan_Turing_Aged_16.jpg/220px-Alan_Turing_Aged_16.jpg" alt="Alan Turing"/>
+          <p><em>Alan Turing (Wikimedia)</em></p>
+        </Col>
+      </Row>
+    </Grid>
   </div>
 )
 
 const Footer = () => (
   <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
-
-    See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code. 
+    <Well>
+      <p>Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>. See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code.</p>
+    </Well>
   </div>
 )
 
-const Notification = ({notification}) => {
-  const notificationStyle = {
-    display: notification === '' ? 'none' : '',
-    padding: '1rem',
-    color: 'darkgreen',
-    border: '1px dashed darkgreen',
-    borderRadius: '1rem',
-    marginTop: '1rem',
-    marginBottom: '1rem',
-    textAlign: 'center'
-  }
+// Tää jääköön nyt tänne osoittamaan että tämäkin tuli tehtyä.
 
-  return (
-  <div style={notificationStyle}>
-    <p>{notification}</p>
-  </div>
-  )
-}
+// const Notification = ({notification}) => {
+//   const notificationStyle = {
+//     display: notification === '' ? 'none' : '',
+//     padding: '1rem',
+//     color: 'darkgreen',
+//     border: '1px dashed darkgreen',
+//     borderRadius: '1rem',
+//     marginTop: '1rem',
+//     marginBottom: '1rem',
+//     textAlign: 'center'
+//   }
+
+//   return (
+//   <div style={notificationStyle}>
+//     <p>{notification}</p>
+//   </div>
+//   )
+// }
 
 class CreateNew extends React.Component {
   constructor() {
@@ -118,19 +149,15 @@ class CreateNew extends React.Component {
       <div>
         <h2>create a new anecdote</h2>
         <form onSubmit={this.handleSubmit}>
-          <div>
-            content 
-            <input name='content' value={this.state.content} onChange={this.handleChange} />
-          </div>
-          <div>
-            author
-            <input name='author' value={this.state.author} onChange={this.handleChange} />
-          </div>
-          <div>
-            url for more info
-            <input name='info' value={this.state.info} onChange={this.handleChange} />
-          </div> 
-          <button>create</button>
+        <FormGroup>
+            <ControlLabel>Content</ControlLabel> 
+            <FormControl name='content' value={this.state.content} onChange={this.handleChange} />
+            <ControlLabel>Author</ControlLabel> 
+            <FormControl name='author' value={this.state.author} onChange={this.handleChange} />
+            <ControlLabel>Url for more info</ControlLabel> 
+            <FormControl name='info' value={this.state.info} onChange={this.handleChange} />
+            <Button bsStyle="success" type="submit">Create</Button>
+        </FormGroup>
         </form>
       </div>  
     )
@@ -191,16 +218,17 @@ class App extends React.Component {
   }
 
   render() {
-
-
-
     return (
-      <div>
+      <div className="container">
         <h1>Software anecdotes</h1>
         <Router>
           <div>
             <Menu />
-            <Notification notification={this.state.notification}/>
+            {(this.state.notification &&
+              <Alert color="success">
+                {this.state.notification}
+              </Alert>
+            )}
             <Route exact path="/" render={() => <AnecdoteList anecdotes={this.state.anecdotes} />} />
             <Route path="/create" render={({history}) => <CreateNew history={history} addNew={this.addNew} />} />
             <Route path="/about" render={() => <About />} />
